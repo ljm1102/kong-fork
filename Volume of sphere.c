3e3dca54de364d1ee81e6df543c207ea465703bb
ljm1102 <127467536+ljm1102@users.noreply.github.com>
2023-03-24 13:56:05 +0900

    add sphere

--- Volume of sphere.c
+++ Volume of sphere.c
@@ -10,12 +10,16 @@
 
 int main(void)
 {
-	float r;
+	float r1,r2;
 
-	printf("Enter the num : ");
-	scanf("%f", &r);
+	printf("Enter the num1 : "); //첫번째 구의 반지름 입력
+	scanf("%f", &r1);
+	
+	printf("Enter the num2 : "); //두번째 구의 반지름 입력
+	scanf("%f", &r2);
 
-	printf("The volume of a sphere with a %f -meter radius is %f", r, 4.0f / 3.0f * 3.14 * r * r * r);
+	printf("The volume of a sphere_1 with a %f -meter radius is %f", r1, 4.0f / 3.0f * 3.14 * r1 * r1 * r1); //첫번째 구의 부피
+	printf("The volume of a sphere_2 with a %f -meter radius is %f", r2, 4.0f / 3.0f * 3.14 * r2 * r2 * r2); //두번째 구의 부피
 
 	return 0;
 }
